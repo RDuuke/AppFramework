9f1fd5212b187c7330a44fa9104a3e96e46d9e0f
RDuuke <juuanduuke@gmail.com>
2016-08-26 13:44:51 -0500

    eliminando archivos

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,8 @@ use Psr\Http\Message\StreamInterface;
 
 $app = new Slim\App();
 
+
+$app->get('/', 'RDuuke\Newbie\Controllers\BaseController:index');
 $app->get('/users', 'RDuuke\Newbie\Controllers\UsersController:index');
 $app->post('/users', 'RDuuke\Newbie\Controllers\UsersController:store');
 $app->get('/users/{id}', 'RDuuke\Newbie\Controllers\UsersController:show');
